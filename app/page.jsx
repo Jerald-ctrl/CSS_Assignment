2027ec72fd2e4d024c5eac19498fa4c4dc6a2427
Jerald-ctrl <134355487+Jerald-ctrl@users.noreply.github.com>
2024-02-05 16:26:58 +0800

    Default config v2

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+function Template() {
+    return (
+        
+      <div>
+        <title>Hi</title>
+        {/*<Header /> 
+        <Sidebar />*/}
+        <div className="content">
+        <p>Hello</p>
+          {/* Your page content goes here */}
+        </div>
+        {/*<Footer /> 
+        <Sidebar />*/}
+      </div>
+    );
+  }
+  
+  export default Template;
